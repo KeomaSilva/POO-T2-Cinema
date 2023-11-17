6c164483432e7d24fd062843606ecb25428d8975
Keoma Silva <122638495+KeomaSilva@users.noreply.github.com>
2023-11-16 22:56:37 -0300

    fixed: tentativa de grid

--- POOT2Cinema/src/Cine/Assento.java
+++ POOT2Cinema/src/Cine/Assento.java
@@ -1,6 +1,8 @@
 package Cine;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Assento implements Serializable {
 
@@ -8,12 +10,39 @@ public class Assento implements Serializable {
 	private int fileira;
 	private int coluna;
 	private Sala sala;
+	private Map<String, Ingresso> ingressos;
 
-	public Assento (Sala sala) {
+	public Assento(Sala sala) {
 		this.fileira = 10;
 		this.coluna = 10;
 		this.sala = sala;
+		ingressos = new HashMap<>();
 	}
+
+	public String lugaresVagos() {
+		StringBuilder str = new StringBuilder();
+		str.append("");
+		for (int i = 11; i >= 1; i--) {
+			if(i == 11) {
+				str.append("Linhas  - ");
+			}else {
+				str.append("Linha " + "i" + " - ");				
+			}
+			for (int j = 0; j <= 10;) {
+				if(j==0) {
+					str.append("- O -");
+				}
+				if() {
+					str.append("- O -");
+				} else {
+					str.append("- X -");
+				}
+			}
+		}
+		str.append("\n----- TELA -----");
+		return str.toString();
+	}
+
 	public Sala getSala() {
 		return sala;
 	}
